06660bbb5a09791c55bf57fd44c7662a9eb1cf18
harishpython1 <harish.python.com@gmail.com>
2019-02-15 09:05:21 +0530

    Muliple browser code commit

---
+++ Login.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+browser='chrome'
+if browser=='chrome':
+    driver=webdriver.Chrome()
+elif browser=='ff':
+    driver=webdriver.Firefox(executable_path="C:/Users/FACULTY/Documents/drivers/geckodriver.exe")
+elif browser=='ie':
+    driver=webdriver.Ie(executable_path="C:/Users/FACULTY/Documents/drivers/IEDriverServer.exe")
+driver.get("http://facebook.com")
+driver.maximize_window()
